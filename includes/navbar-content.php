4061cdea4a4fa3256ec0949fd1a9cda04d42ad79
Adedayo Ayomide <adefeyisayo998@gmail.com>
2024-01-12 13:37:32 +0100

    designed the navbar for the theme

---
+++ includes/navbar-content.php
@@ -0,0 +1,37 @@
+<div class="vg-page page-home" id="home" style="background-image: url(./assets/images/some.jpg); background-repeat: no-repeat; background-size: cover; width: 100%; height: 100vh;">
+        <!-- Navbar -->
+        <div class="navbar navbar-expand-lg navbar-dark sticky" data-offset="500">
+          <div class="container">
+            <a href="" class="navbar-brand">Samuel</a>
+            <button class="navbar-toggler" data-toggle="collapse" data-target="#main-navbar" aria-expanded="true">
+              <span class="ti-menu"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="main-navbar">
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                  <a href="#home" class="nav-link" data-animate="scrolling">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a href="#about" class="nav-link" data-animate="scrolling">About</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#resume" class="nav-link" data-animate="scrolling">Resume</a>
+                  </li>
+                <li class="nav-item">
+                  <a href="#portfolio" class="nav-link" data-animate="scrolling">Portfolio</a>
+                </li>
+                <li class="nav-item">
+                  <a href="#blog" class="nav-link" data-animate="scrolling">Blog</a>
+                </li>
+                <li class="nav-item">
+                  <a href="#contact" class="nav-link" data-animate="scrolling">Contact</a>
+                </li>
+              </ul>
+              <ul class="nav ml-auto">
+                <li class="nav-item">
+                  <button class="btn btn-fab btn-theme no-shadow">En</button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div> <!-- End Navbar -->
