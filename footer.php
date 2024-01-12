44c9f534ed4240631086f6dadcc82dcb8ff21807
Adedayo Ayomide <adefeyisayo998@gmail.com>
2024-01-12 13:35:30 +0100

    designed the footer.php

---
+++ footer.php
@@ -0,0 +1,15 @@
+<?php wp_footer();?>
+  <!-- footer -->
+  <div class="container">
+        <footer class="footer">       
+            <p class="mb-0">Copyright <script>document.write(new Date().getFullYear())</script> &copy; <a href="http://www.codemygig.com">CodeMyGig</a></p>
+            <div class="social-links text-right m-auto ml-sm-auto">
+                <a href="https://github.com/Haywhyoh" class="link"><i class="ti-github"></i></a>
+                <a href="https://twitter.com/cruise_dev" class="link"><i class="ti-twitter-alt"></i></a>
+                <!-- <a href="javascript:void(0)" class="link"><i class="ti-google"></i></a> -->
+                <a href="https://www.linkedin.com/in/ayomide-samuel/" class="link"><i class="ti-linkedin"></i></a>
+                <!-- <a href="javascript:void(0)" class="link"><i class="ti-instagram"></i></a> -->
+                <a href="https://www.writeaboutcoding.com" class="link"><i class="ti-rss"></i></a>
+            </div>
+        </footer>
+    </div> <!-- end of page footer -->
