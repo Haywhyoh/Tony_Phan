86be63e5d4d3df18d8925bf86908a48a36de001e
Adedayo Ayomide <adefeyisayo998@gmail.com>
2024-01-12 13:35:16 +0100

    creatd the header for the team

---
+++ header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+  <title><?php the_title() ?></title>
+
+  <!-- <link rel="shortcut icon" href="./assets/favicon.ico" type="image/x-icon">
+  
+  <link rel="stylesheet" type="text/css" href="./assets/css/themify-icons.css">
+  
+  <link rel="stylesheet" type="text/css" href="./assets/css/bootstrap.css">
+
+  <link rel="stylesheet" type="text/css" href="./assets/vendor/animate/animate.css">
+  
+  <link rel="stylesheet" type="text/css" href="./assets/vendor/owl-carousel/owl.carousel.css">
+  
+  <link rel="stylesheet" type="text/css" href="./assets/vendor/perfect-scrollbar/css/perfect-scrollbar.css">
+  
+  <link rel="stylesheet" type="text/css" href="./assets/vendor/nice-select/css/nice-select.css">
+  
+  <link rel="stylesheet" type="text/css" href="./assets/vendor/fancybox/css/jquery.fancybox.min.css">
+  
+  <link rel="stylesheet" type="text/css" href="./assets/css/virtual.css">
+  
+  <link rel="stylesheet" type="text/css" href="./assets/css/topbar.virtual.css"> -->
+</head>
+
+    <!-- Topbar End -->
+<body class="theme-red">
+
+
+<?php get_template_part('includes/navbar', 'content');?>
+
+    <?php wp_head();?>
