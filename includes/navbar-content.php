1c1c4522789172b7f46579eb9bb1e15783b7cfbb
Ayomide <adefeyisayo998@gmail.com>
2024-01-15 11:11:56 +0100

    added header class

--- includes/navbar-content.php
+++ includes/navbar-content.php
@@ -7,6 +7,41 @@
         <span class="ti-menu"></span>
       </button>
       <div class="collapse navbar-collapse" id="main-navbar">
+
+      <?php
+          function custom_primary_menu_fallback() {
+            ?>
+             <div class="collapse navbar-collapse" id="main-navbar">
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                  <a href="#home" class="nav-link" data-animate="scrolling">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a href="#about" class="nav-link" data-animate="scrolling">About</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#resume" class="nav-link" data-animate="scrolling">Resume</a>
+                  </li>
+                <li class="nav-item">
+                  <a href="#portfolio" class="nav-link" data-animate="scrolling">Portfolio</a>
+                </li>
+                <li class="nav-item">
+                  <a href="#blog" class="nav-link" data-animate="scrolling">Blog</a>
+                </li>
+                <li class="nav-item">
+                  <a href="#contact" class="nav-link" data-animate="scrolling">Contact</a>
+                </li>
+              </ul>
+              <ul class="nav ml-auto">
+                <li class="nav-item">
+                  <button class="btn btn-fab btn-theme no-shadow">En</button>
+                </li>
+              </ul>
+            </div>
+           <?php
+          }
+      ?>
+
       <?php
       wp_nav_menu(
         array(
@@ -17,6 +52,7 @@
           'menu_class' => 'navbar-nav ml-auto',
           'add_li_class' => 'nav-item',
           'add_a_class' => 'nav-link',
+          'fallback_cb' => 'custom_primary_menu_fallback',
         )
       )
       ?>
