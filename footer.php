6d071766f6850ac320f2c783964f2e0890f679b4
Adedayo Ayomide <adefeyisayo998@gmail.com>
2024-01-18 14:28:56 +0100

    addded footer links

--- footer.php
+++ footer.php
@@ -6,12 +6,8 @@
             <div class="mb-0">Copyright <script>document.write(new Date().getFullYear())</script> &copy; <a href="">Tony_Phan Marketing</a></div>
             <div class='ml-5' ><a href="https://codemygig.com" style="color:#F2E5B7 !important">Designed by CodeMyGig</a></div>
             <div class="social-links text-right m-auto ml-sm-auto">
-                <a href="https://github.com/Haywhyoh" class="link"><i class="ti-facebook"></i></a>
-                <a href="https://twitter.com/cruise_dev" class="link"><i class="ti-twitter-alt"></i></a>
-                <!-- <a href="javascript:void(0)" class="link"><i class="ti-google"></i></a> -->
                 <a href="https://www.linkedin.com/in/ayomide-samuel/" class="link"><i class="ti-linkedin"></i></a>
-                <!-- <a href="javascript:void(0)" class="link"><i class="ti-instagram"></i></a> -->
-                <a href="https://www.writeaboutcoding.com" class="link"><i class="ti-youtube"></i></a>
+                <a href="https://vimeo.com/showcase/8530841" class="link"><i class="ti-vimeo"></i></a>
             </div>
         </footer>
     </div> <!-- end of page footer -->
