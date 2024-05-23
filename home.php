0fb97a13adbd0342ee0d5537c9f30be2b100954d
Martins Anerua <aneruamartins@gmail.com>
2024-05-23 13:45:38 +0100

    refactor: remove testimonial section

--- home.php
+++ home.php
@@ -33,12 +33,9 @@
                     <?php echo nl2br(esc_html(get_theme_mod('about_me'))); ?>
                 </p>
                 <div class="d-flex gap-x-4">
-
                     <button class="btn-rounded btn btn-outline-primary mt-4">
                         <div><a href="<?php echo esc_url(get_theme_mod('cv_pdf_file')); ?>" download>Download CV</a></div>
-
                     </button>
-=
                 </div>
             </div>
         </div><!-- end of about wrapper -->
@@ -265,42 +262,6 @@
 
     </div>
 </section>
-
-<!-- testimonial section -->
-<section class="section wow slideInUp" id="testmonial">
-    <div class="container text-center">
-        <p class="section-subtitle">What Think Client About Me ?</p>
-        <h6 class="section-title mb-6">Testmonial</h6>
-
-        <!-- row -->
-        <div class="row">
-            <?php
-            $testimonial_query = new WP_Query(array(
-                "post_type" => 'testimonial',
-                "posts_per_page" => -1
-            ));
-            if ($testimonial_query->have_posts()) {
-                while ($testimonial_query->have_posts()) {
-                    $testimonial_query->the_post();
-            ?>
-                    <div class="col-md-6">
-                        <div class="testimonial-card">
-                            <div class="testimonial-card-img-holder">
-                                <img src="<?php echo the_field('testimonial_image') ?>" class="testimonial-card-img" alt="">
-                            </div>
-                            <div class="testimonial-card-body">
-                                <p class="testimonial-card-subtitle"><?php echo the_field('testimonial') ?></p>
-                                <h6 class="testimonial-card-title"><?php echo the_field('reviewer') ?></h6>
-                            </div>
-                        </div>
-                    </div>
-            <?php
-                }
-            }
-            ?>
-        </div>
-    </div> <!-- end of container -->
-</section> <!-- end of testimonial section -->
 
 <!-- contact section -->
 <section class="section wow slideInUp" id="contact" ">
