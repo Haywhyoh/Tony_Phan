3a8facbd6a4cb0a173d1882346fd6c33f5a0a9f8
Ayomide <adefeyisayo998@gmail.com>
2024-05-23 14:15:05 +0100

    separated testimony

--- home.php
+++ home.php
@@ -274,41 +274,9 @@
     </div>
 </section>
 
-<!-- testimonial section -->
-<section class="section wow slideInUp" id="testmonial">
-    <div class="container text-center">
-        <p class="section-subtitle">What Think Client About Me ?</p>
-        <h6 class="section-title mb-6">Testmonial</h6>
 
-        <!-- row -->
-        <div class="row">
-            <?php
-            $testimonial_query = new WP_Query(array(
-                "post_type" => 'testimonial',
-                "posts_per_page" => -1
-            ));
-            if ($testimonial_query->have_posts()) {
-                while ($testimonial_query->have_posts()) {
-                    $testimonial_query->the_post();
-            ?>
-                    <div class="col-md-6">
-                        <div class="testimonial-card">
-                            <div class="testimonial-card-img-holder">
-                                <img src="<?php echo the_field('testimonial_image') ?>" class="testimonial-card-img" alt="">
-                            </div>
-                            <div class="testimonial-card-body">
-                                <p class="testimonial-card-subtitle"><?php echo the_field('testimonial') ?></p>
-                                <h6 class="testimonial-card-title"><?php echo the_field('reviewer') ?></h6>
-                            </div>
-                        </div>
-                    </div>
-            <?php
-                }
-            }
-            ?>
-        </div>
-    </div> <!-- end of container -->
-</section> <!-- end of testimonial section -->
+
+<!-- end of testimonial section -->
 
 <!-- contact section -->
 <section class="section wow slideInUp" id="contact" ">
