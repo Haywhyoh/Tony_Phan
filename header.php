f8422cb975a00f01113ec14ff19de40b1ba66fd3
Ayomide <adefeyisayo998@gmail.com>
2024-05-24 08:27:36 +0100

    added meta description and data

--- header.php
+++ header.php
@@ -1,8 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="A virtual Production Expert, Production Manager and Digital Content Creator with over 9 years of experience">
+
+
+    <title>Sean Sullivan</title>
 
 </head>
 
