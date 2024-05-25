130a9230a529b8ab70b33f7b50ff33a3eff38a21
Martins Anerua <aneruamartins@gmail.com>
2024-05-25 15:08:22 +0100

    feat: change footer design

--- footer.php
+++ footer.php
@@ -1,15 +1,22 @@
+<!-- footer -->
+<div class="footer-container">
+    <!-- Designed by CodeMyGig, https://codemygig.com -->
+    <footer class="container footer py-3">
+        <div class="col-6">
+            Copyright
+            <script>
+                document.write(new Date().getFullYear())
+            </script>
+            &nbsp;&copy;&nbsp;
+            <a href="/">Sean Sullivan</a>
+        </div>
+        <div class="col-6 social-links text-right">
+            <a href="https://twitter.com/se_sully" class="link"><i class="ti-twitter-alt"></i></a>
+            <a href="https://www.facebook.com/sean.e.sullivan" class="link"><i class="ti-facebook"></i></a>
+            <a href="https://www.linkedin.com/in/sesully" class="link"><i class="ti-linkedin"></i></a>
+        </div>
+    </footer>
+</div>
+<!-- end of page footer -->
 
-  <!-- footer -->
-
-  <div class="container">
-        <footer class="footer">       
-            <div class="mb-0">Copyright <script>document.write(new Date().getFullYear())</script> &copy; <a href="">Sean Sullivan</a></div>
-            <div class='ml-5' ><a href="https://codemygig.com" style="color:#f7f7f8 !important">Designed by CodeMyGig</a></div>
-            <div class="social-links text-right m-auto ml-sm-auto">
-                <a href="https://www.linkedin.com/in/ayomide-samuel/" class="link"><i class="ti-linkedin"></i></a>
-                <a href="https://vimeo.com/showcase/8530841" class="link"><i class="ti-vimeo"></i></a>
-            </div>
-        </footer>
-    </div> <!-- end of page footer -->
-
-    <?php wp_footer();?>
+<?php wp_footer(); ?>
